8647535fa11fcbad1e567f4562803277530d2190
Salvatore949 <86173998+Salvatore949@users.noreply.github.com>
2021-11-10 23:16:30 +0100

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -1,77 +1,83 @@
-// Attraverso un append genero la griglia 
-// Attraverso un prompt chiedo all'utente quale livello di difficoltà vuole selezionare
-// Creo delle condizioni in modo da regolare il numero di quadrati
-// Utilizzo una function per far colorare le caselle
-// Creo un array di 16 numeri casuali utilizzando Math.floor e un array
-// Evito che ci siano doppi numeri entro l'array tramite una condizione
-// Creo una condizione
+const gridCont = document.getElementById("grid");
+const levl = parseInt(prompt("livello?"));
+var points = 0;
 
-const squareCont = document.querySelector('#containerSquare');
+// const generaElemento = (elementGen, classPlus) => {
+//     let node = document.createElement(elementGen);
+//     node.classList.add(classPlus);
+//     return node
+// }
 
-const Livelli = parseInt(prompt(" '1'-'2'-'3' ")) 
+// genero l'array bombe
+const bombList = bombGen(16, 1, levl);
+// console.log(bombList);
 
-let difficolta = 100;
+for(let i = 1; i <= 64; i++){
 
-if (Livelli == 1) {
-    difficolta = 100;
+    let newElem = generaElemento("div", "square", i);    
 
-}
-
-if (Livelli == 2) {
-    difficolta = 81;
-
-}
-
-if (Livelli == 3) {
-    difficolta = 49;
-
-}
-
-for (let i = 1; i <= difficolta; i++){
-
-  let Element = document.createElement('div');
-  Element.classList.add('square');
-  Element.append(i);
-  squareCont.appendChild(Element);
-
-  Element.addEventListener("click",
+    newElem.addEventListener("click",
         function(){
-            Element.classList.add("clicked_true")
+            // newElem.classList.add("clicked-true");
+            console.log(this);
+            this.classList.add("clicked-true");
+
+            let numInt = parseInt(this.innerText);
+
+            // se è una bomba agiungo altra classe
+            if(bombList.includes(numInt)){
+                this.classList.add("boom");
+                console.log("BOOOM! FINE GIOCO!!, hai totalizzato punti: ", points);
+            } else {
+                ++points;
+                console.log("BRAVO/A!!! punteggio momentaneo a:", points);
+            }
+            // console.log(this.innerText);
+            // sennò aggiungo punto
         }
-  );
+
+
+        // () => {
+        //     // newElem.classList.add("clicked-true");
+        //     console.log(this);
+        //     this.classList.add("clicked-true");
+        // }
+    );
+
+    gridCont.appendChild(newElem);
 }
 
-//Creo un array di 16 numeri casuali che non si ripetono
-const arrNum = [];
-while(arrNum.length < 16) { 
-    let num = (Math.floor(Math.random()*difficolta)+1);
-    if (arrNum.includes(num) === false){
-        arrNum.push(num)
-    }
-    console.log(arrNum)
- }
 
 
-function inizio(livello, classe) {
-    for ( let i = 1; i <= livello; i++) {
 
-        let nuoveCelle = creareCelle("div", classe);
-        nuoveCelle.id = i;
-        contenitoreCelle.append(nuoveCelle);
+// funzioni utili
 
-        nuoveCelle.addEventListener("click",
-            function(){
-                nuoveCelle.classList.add("cliccato");
-                nuoveCelle.innerText = i;
-                const id = parseInt(nuoveCelle.id);
-                console.log('hai clickato su id ', id);
-
-                if(bombe.includes(id)) {
-                    nuoveCelle.classList.add("bomba");
-                    alert ("Hai perso");
-                }
-            }   
-        )
-    }
+//  crea elemento del DOM
+function generaElemento(elementGen, classPlus, tex) {
+    let node = document.createElement(elementGen);
+    node.append(tex);
+    node.classList.add(classPlus);
+    return node
 }
+
+// crea array di numeri bomba
+function bombGen (bombnum, min, max){
+    let arrBombs = [];
+    while(arrBombs.length < bombnum){
+        let num = getRandomInt(min, max);
+        if(!arrBombs.includes(num)){
+            arrBombs.push(num);
+        }
+    }
+    return arrBombs
+
+}
+
+// crea numeri random tra min e max
+function getRandomInt(min, max) {
+    min = Math.ceil(min) || 0;
+    max = Math.floor(max) || Number.MAX_SAFE_INTEGER;
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result;
+  }
 
